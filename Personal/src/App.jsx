478fedb8f89412b19f2f07123f15ddcb865055e5
Austin Mazur <mazuraustin1@gmail.com>
2023-06-03 21:38:15 +0200

    Bjj tracker app, create first render of cards

--- Personal/src/App.jsx
+++ Personal/src/App.jsx
@@ -1,13 +1,24 @@
-
+import logs from "../db.json/";
+import { useState } from "react";
+import Card from "./components/Card";
 
 function App() {
+  const [showDetails, setShowDetails] = useState(true);
+  const [bjjLogs, setBjjLogs] = useState(logs);
 
-
+  const handleClick = (logId) => {
+    const updatedLogs = bjjLogs.map((log) =>
+      log.id === logId ? { ...log, showDetails: !log.showDetails } : log
+    );
+    setBjjLogs(updatedLogs);
+  };
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Bjj Tracker</h1>
+      {console.log(bjjLogs)}
+      <Card bjjLogs={bjjLogs} handleClick={handleClick} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Personal/src/components/CardDetails.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const CardDetails = ({ log, handleClick }) => {
+  return (
+    <div>
+        <div onClick={() => handleClick(log.id)} className='flex flex-col items-center bg-blue-200 rounded-lg p-2' key={log.id}>
+          <h3 className='font-bold'>{log.position}</h3>
+          <p>{log.date}</p>
+          <p>{log.Gi ? log.Gi : log.NoGi}</p>
+          {log.showDetails && (
+          <div className='flex flex-col items-center'>
+            <p>{log.offense ? log.offense : log.defense}</p>
+            <p className='border-2 p-2 rounded-xl border-slate-100'>{log.technique}</p>
+            <p className='font-medium'>{log.school}</p>
+            <p>{log.teacher}</p>
+          </div>
+        )}
+        </div>
+    </div>
+  )
+}
+
+export default CardDetails
